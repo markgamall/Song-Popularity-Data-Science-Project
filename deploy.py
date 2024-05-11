c2a776f9ae8ac62b7c70737e57d34753971f7baf
Mark Gamal <113033083+markgamall@users.noreply.github.com>
2024-05-11 20:31:28 +0300

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,34 +1,11 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+
 import os
-import re
+
 import ast
-from scipy import stats
-from sklearn.model_selection import GridSearchCV
-import xgboost as xgb
-from sklearn.model_selection import KFold, cross_val_score
-from sklearn.linear_model import Lasso
-from sklearn.linear_model import Ridge
-from sklearn.preprocessing import StandardScaler
-from scipy.stats import uniform, randint
-from sklearn.model_selection import train_test_split, RandomizedSearchCV
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.pipeline import make_pipeline
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import LinearRegression
-from sklearn.svm import SVR
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.model_selection import RandomizedSearchCV
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import mean_squared_error , r2_score,accuracy_score,mean_absolute_error
-import plotly.express as px
-from sklearn.feature_selection import SelectKBest, f_regression, chi2, mutual_info_regression,f_classif
-from scipy.stats import pearsonr, spearmanr
-from category_encoders import TargetEncoder
-from sklearn.preprocessing import LabelEncoder
+
 import pickle
 
 # Set the CSS styling
